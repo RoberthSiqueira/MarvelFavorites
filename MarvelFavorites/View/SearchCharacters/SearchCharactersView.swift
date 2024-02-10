9a19725e15e6c25f39758fdfc1f5766e8f8cdd22
Roberth Siqueira <roberth.siqueira@gmail.com>
2024-02-10 17:47:32 -0300

    feat: Add SearchBar on View

--- MarvelFavorites/View/SearchCharacters/SearchCharactersView.swift
+++ MarvelFavorites/View/SearchCharacters/SearchCharactersView.swift
@@ -1,4 +1,46 @@
 import UIKit
 
+protocol SearchCharactersViewDelegate: AnyObject {}
+
 class SearchCharactersView: UIView {
+
+    // MARK: - PROPERTIES
+
+    weak var delegate: SearchCharactersViewDelegate?
+
+    // MARK: - UI
+
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.enablesReturnKeyAutomatically = true
+        searchBar.delegate = self
+        searchBar.placeholder = "Character's name"
+        return searchBar
+    }()
+
+    // MARK: - API
+
+    func setupView() {
+        backgroundColor = .white
+        addViewHierarchy()
+    }
+
+    // MARK: - VIEW
+
+    private func addViewHierarchy() {
+        addSubview(searchBar)
+
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        let safeArea = safeAreaLayoutGuide
+
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
 }
