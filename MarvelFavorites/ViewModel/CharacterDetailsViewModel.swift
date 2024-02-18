bb364cab3b709926a70672769cb327e8d854c2a0
Roberth Siqueira <roberth.siqueira@gmail.com>
2024-02-18 13:36:32 -0300

    feat: add favorite object

--- MarvelFavorites/ViewModel/CharacterDetailsViewModel.swift
+++ MarvelFavorites/ViewModel/CharacterDetailsViewModel.swift
@@ -1,18 +1,52 @@
+import CoreData
 import Foundation
 
 class CharacterDetailsViewModel {
 
+    // MARK: PROPERTIES
+
+    private let viewContext = DataController.shared.viewContext
     private let character: CharacterViewModel
+
+    // MARK: - INIT
 
     init(character: CharacterViewModel) {
         self.character = character
     }
+
+    // MARK: - API
 
     func retrieveCharacter() -> CharacterViewModel {
         return character
     }
 
     func toFavorite(_ alreadyIs: Bool) {
-        // TODO: Add on CoreData if it is not favorite OR Remove from objects on CoreData if it already favorite
+        if alreadyIs {
+            // TODO: Remove objects from View Context
+        }
+        createFavorite()
+    }
+
+    // MARK: METHODS
+
+    private func createFavorite() {
+        let favorite = Favorite(context: viewContext)
+        favorite.name = character.name
+        favorite.desc = character.description
+        favorite.imageURL = character.imageURL
+        favorite.comics = Int16(character.comics)
+        favorite.events = Int16(character.events)
+        favorite.series = Int16(character.series)
+        favorite.stories = Int16(character.stories)
+
+        saveContext()
+    }
+
+    private func saveContext() {
+        do {
+            try viewContext.save()
+        } catch {
+            fatalError("Could not save favorite")
+        }
     }
 }
