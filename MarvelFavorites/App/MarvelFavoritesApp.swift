26bf775e852d738a91acacc9782e5015d1c76916
Roberth Siqueira <roberth.siqueira@gmail.com>
2025-03-29 11:30:30 -0300

    feat: initializes by swiftui

---
+++ MarvelFavorites/App/MarvelFavoritesApp.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+@main
+struct MarvelFavoritesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MarvelFavorites()
+        }
+    }
+}
+
+struct MarvelFavorites: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MarvelFavorites()
+}
